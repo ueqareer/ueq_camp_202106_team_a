44a4a1d9d5b7cc9985d1f3ac914f32f073feaa28
Ryuki Goto <rth510ryu@gmail.com>
2021-08-07 22:22:50 +0900

    Homepage-layoutの作成

--- lib/screens/screen3.dart
+++ lib/screens/screen3.dart
@@ -1,14 +1,102 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:cordinate_sns_app/widgets/neumorphic_bottom_navigation.dart';
 
 class Screen3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.yellow,
-        title: Text('Screen 3'),
+    return SafeArea(
+      child: Scaffold(
+        appBar: NeumorphicAppBar(
+          title: NeumorphicText(
+            "Home-layout",
+            textStyle: NeumorphicTextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30.0,
+            ),
+            style: NeumorphicStyle(
+              color: Colors.black,
+            ),
+          ),
+        ),
+        body: ListView.builder(
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          itemBuilder: _buildVerticalItem,
+        ),
+        floatingActionButton: NeumorphicFloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: (){},
+        ),
+        bottomNavigationBar: NeumorphicBottomNavigation(),
       ),
-      body: Center(),
     );
   }
 }
+
+Widget _buildVerticalItem(BuildContext context, int verticalIndex) {
+  return Neumorphic(
+    style: NeumorphicStyle(
+      boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(10)),
+      shape: NeumorphicShape.concave,
+      depth: 5,
+      intensity: 0.65,
+      lightSource: LightSource.topLeft,
+    ),
+    child: Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      child: SizedBox(
+        height: 320,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: 200,
+              height: 50,
+              // color: Colors.white70,
+              margin: EdgeInsets.fromLTRB(5, 20, 20, 20),
+              child: ListTile(
+                leading:NeumorphicIcon(
+                  Icons.person,
+                  size: 45,
+                ),
+                title: Text("User Name",
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black
+                  ),
+                ),
+              ),
+            ),
+            _buildHorizontalItem(context, verticalIndex),
+          ],
+        ),
+      ),
+    ),
+  );
+}
+
+
+Widget _buildHorizontalItem(BuildContext context, int verticalIndex) {
+  return SizedBox(
+    height: 200,
+    child: PageView.builder(
+      controller: PageController(viewportFraction: 0.7),
+      itemBuilder: (context, horizontalIndex) =>
+          _buildHorizontalView(context, verticalIndex, horizontalIndex),
+    ),
+  );
+}
+
+Widget _buildHorizontalView(
+    BuildContext context, int verticalIndex, int horizontalIndex) {
+  final imageUrl =  'https://source.unsplash.com/random';
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 8),
+    child: Card(
+      child: Image.network(imageUrl),
+    ),
+  );
+}
+
+
